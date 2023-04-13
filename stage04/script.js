1749b9997cd702c9b44feebdcb148eeadbf0005a
João <joaovictor.ramos34@gmail.com>
2023-04-13 15:48:34 -0300

    terceiro exercicio

--- stage04/script.js
+++ stage04/script.js
@@ -1,8 +1,22 @@
 /*
-    Solicite 2 números, faça a soma deles e apresnete o resultado final ao usuário
-*/
+    Capture 2 números e faça as operações matemáticas de: 
+    soma, subtração, divisão, multiplicação e resto da divisão!
+ */
 
-let numOne = prompt("Digite o primeiro número: ");
-let numTwo = prompt("Digite o segundo número: ");
-let result = Number(numOne) + Number(numTwo);
-alert(`A soma dos números ${numOne} e ${numTwo} é: ${result} `)
+let firstNumber = prompt("Digite o primeiro número: ");
+let secondNumber = prompt("Digite o segundo número: ");
+
+firstNumber = Number(firstNumber);
+secondNumber = Number(secondNumber);
+
+const sum = firstNumber + secondNumber;
+const sub = firstNumber - secondNumber;
+const multi = firstNumber * secondNumber;
+const div = firstNumber / secondNumber;
+const rest = firstNumber % secondNumber;
+
+alert("Soma: " + sum);
+alert("Subtração: " + sub);
+alert("multiplicação: " + multi);
+alert("Divisão: " + div);
+alert("Resto: " + rest);
---
+++ stage04/script02.js
@@ -0,0 +1,8 @@
+/*
+    Solicite 2 números, faça a soma deles e apresnete o resultado final ao usuário
+*/
+
+let numOne = prompt("Digite o primeiro número: ");
+let numTwo = prompt("Digite o segundo número: ");
+let result = Number(numOne) + Number(numTwo);
+alert(`A soma dos números ${numOne} e ${numTwo} é: ${result} `)
