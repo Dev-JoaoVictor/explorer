7dd51f92f87d629573a527c23f6c78dc26e39988
João <joaovictor.ramos34@gmail.com>
2023-04-16 12:55:37 -0300

    Criaação da lista de estudantes

---
+++ stage04/desafio02.js
@@ -0,0 +1,17 @@
+const listStudents = [
+  {
+    name: "João",
+    firstTest: 7.3,
+    secondTest: 9.2,
+  },
+  {
+    name: "Pedro",
+    firstTest: 5.3,
+    secondTest: 8.2,
+  },
+  {
+    name: "Marcos",
+    firstTest: 4.3,
+    secondTest: 6.2,
+  },
+];
