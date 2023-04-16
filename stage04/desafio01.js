d945d7795c902596a8bd9c906261d521d3a5b8db
João <joaovictor.ramos34@gmail.com>
2023-04-16 12:22:53 -0300

    Criando as condicionais

--- stage04/desafio01.js
+++ stage04/desafio01.js
@@ -14,5 +14,15 @@ alert(`A multiplicação dos dois números é: ${multi}`);
 alert(`A divisão dos dois números é: ${div}`);
 alert(`O resto da divisão dos dois números é: ${rest}`);
 
-
+if (sum % 2 === 0) {
+    alert(`A soma dos dois números é par: ${sum}`);
+  } else {
+    alert(`A soma dos dois números é impar: ${sum}`);
+  }
+  
+  if (numOne == numSecond) {
+    alert("Os número inseridos são iguais");
+  } else {
+    alert("Os número inseridos são diferentes");
+  }
 
