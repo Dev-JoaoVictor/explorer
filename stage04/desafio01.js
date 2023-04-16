675ff19e314df41ffe30044e08ea95eaaa90c077
João <joaovictor.ramos34@gmail.com>
2023-04-16 12:22:14 -0300

    Criando as operações aritméticas

--- stage04/desafio01.js
+++ stage04/desafio01.js
@@ -1,3 +1,8 @@
 numOne = Number(prompt("Digite o primeiro número: "));
 numSecond = Number(prompt("Digite o segundo número: "));
 
+let sum = numOne + numSecond;
+let sub = numOne - numSecond;
+let multi = numOne * numSecond;
+let div = numOne / numSecond;
+let rest = numOne % numSecond;
