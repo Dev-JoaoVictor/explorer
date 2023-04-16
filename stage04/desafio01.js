9c8c882262d0378224abf401403f47acfcd56375
João <joaovictor.ramos34@gmail.com>
2023-04-16 12:22:37 -0300

    Criando os alertas com os resultados

--- stage04/desafio01.js
+++ stage04/desafio01.js
@@ -6,3 +6,13 @@ let sub = numOne - numSecond;
 let multi = numOne * numSecond;
 let div = numOne / numSecond;
 let rest = numOne % numSecond;
+
+
+alert(`A soma dos dois números é: ${sum}`);
+alert(`A subtração dos dois números é: ${sub}`);
+alert(`A multiplicação dos dois números é: ${multi}`);
+alert(`A divisão dos dois números é: ${div}`);
+alert(`O resto da divisão dos dois números é: ${rest}`);
+
+
+
