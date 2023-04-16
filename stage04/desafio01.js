b95a1f474012c3b2316070cbc55aebec5b8a571f
João <joaovictor.ramos34@gmail.com>
2023-04-16 12:25:09 -0300

    Comentando o código

--- stage04/desafio01.js
+++ stage04/desafio01.js
@@ -1,28 +1,31 @@
+/* input de valores*/
 numOne = Number(prompt("Digite o primeiro número: "));
 numSecond = Number(prompt("Digite o segundo número: "));
 
+/* Operações aritméticas*/
 let sum = numOne + numSecond;
 let sub = numOne - numSecond;
 let multi = numOne * numSecond;
 let div = numOne / numSecond;
 let rest = numOne % numSecond;
 
-
+/* Mensagens com resultado das operações*/
 alert(`A soma dos dois números é: ${sum}`);
 alert(`A subtração dos dois números é: ${sub}`);
 alert(`A multiplicação dos dois números é: ${multi}`);
 alert(`A divisão dos dois números é: ${div}`);
 alert(`O resto da divisão dos dois números é: ${rest}`);
 
+/*Condicional para números impares e pares*/
 if (sum % 2 === 0) {
-    alert(`A soma dos dois números é par: ${sum}`);
-  } else {
-    alert(`A soma dos dois números é impar: ${sum}`);
-  }
-  
-  if (numOne == numSecond) {
-    alert("Os número inseridos são iguais");
-  } else {
-    alert("Os número inseridos são diferentes");
-  }
+  alert(`A soma dos dois números é par: ${sum}`);
+} else {
+  alert(`A soma dos dois números é impar: ${sum}`);
+}
 
+/*Condicional para valores inseridos iguáis*/
+if (numOne == numSecond) {
+  alert("Os número inseridos são iguais");
+} else {
+  alert("Os número inseridos são diferentes");
+}
