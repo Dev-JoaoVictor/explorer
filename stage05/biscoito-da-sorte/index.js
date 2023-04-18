1ddb2dc34714a7351963e5a9f34c2f468761f3f3
João <joaovictor.ramos34@gmail.com>
2023-04-18 14:14:30 -0300

    Adicionando as mensagens do biscoito

--- stage05/biscoito-da-sorte/index.js
+++ stage05/biscoito-da-sorte/index.js
@@ -4,3 +4,16 @@ const biscoito = document.querySelector(".biscoito")
 const message = screen2.querySelector("p")
 const reset = screen2.querySelector('button')
 
+const messages = [
+    "A vida trará coisas boas se tiver paciência.",
+    "Não compense na ira o que lhe falta na razão.",
+    "Defeitos e virtudes são apenas dois lados da mesma moeda.",
+    "A juventude não é uma época da vida, é um estado de espírito.",
+    "Siga os bons e aprenda com eles.",
+    "O riso é a menor distância entre duas pessoas.",
+    "Amizade e Amor são coisas que se unem num piscar de olhos.",
+    "Quem olha para fora sonha; quem olha para dentro acorda.",
+    "Você sempre será a sua melhor companhia!",
+    "Realize o óbvio, pense no improvável e conquiste o impossível.",
+    "O Palmeiras não tem mundial"
+]
