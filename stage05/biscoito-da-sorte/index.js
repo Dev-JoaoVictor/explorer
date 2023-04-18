c00d88f6a6c4e01315f56734f92b845386c10062
João <joaovictor.ramos34@gmail.com>
2023-04-18 14:14:01 -0300

    Criando as constantes de controle do dom

---
+++ stage05/biscoito-da-sorte/index.js
@@ -0,0 +1,6 @@
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const biscoito = document.querySelector(".biscoito")
+const message = screen2.querySelector("p")
+const reset = screen2.querySelector('button')
+
