f18ff4fd9fdba0c514d7ce446eee0e529343e100
João <joaovictor.ramos34@gmail.com>
2023-04-20 10:18:38 -0300

    function that cancels form submission

--- stage05/IMC/index.js
+++ stage05/IMC/index.js
@@ -5,3 +5,10 @@ const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
 
+//Funções
+form.onsubmit = (event) => {
+    event.preventDefault() // Tira o recarregamento da página
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+}
