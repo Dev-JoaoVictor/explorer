251acb0b0638b6665e0c1ea7ac97eb85a9fda558
João <joaovictor.ramos34@gmail.com>
2023-04-20 10:14:12 -0300

    Criando as váriaveis de controle

---
+++ stage05/IMC/index.js
@@ -0,0 +1,7 @@
+//Varáveis - variables
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+
