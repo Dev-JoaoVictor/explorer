8d14631ae6e74fb0ba47acfb9de07d6edc3b96d0
João <joaovictor.ramos34@gmail.com>
2023-04-20 10:23:59 -0300

    BMI calculation function

--- stage05/IMC/index.js
+++ stage05/IMC/index.js
@@ -1,14 +1,17 @@
 //Varáveis - variables
 
-const form = document.querySelector('form')
-const inputWeight = document.querySelector('#weight')
-const inputHeight = document.querySelector('#height')
-
+const form = document.querySelector("form");
+const inputWeight = document.querySelector("#weight");
+const inputHeight = document.querySelector("#height");
 
 //Funções
 form.onsubmit = (event) => {
-    event.preventDefault() // Tira o recarregamento da página
+  event.preventDefault(); // Tira o recarregamento da página
 
-    const weight = inputWeight.value
-    const height = inputHeight.value
+  const weight = inputWeight.value;
+  const height = inputHeight.value;
+};
+
+function IMC(weight, height) {
+    return (weight / ((height / 100) * 2)).toFixed
 }
