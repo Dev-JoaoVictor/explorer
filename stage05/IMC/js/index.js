8cb22df7f4173b67731e7e2f4c148c98d00235ae
João <joaovictor.ramos34@gmail.com>
2023-04-21 12:36:14 -0300

    Modularizando o alert error

--- stage05/IMC/js/index.js
+++ stage05/IMC/js/index.js
@@ -1,4 +1,5 @@
 import { modal } from "./modal.js";
+import { AlertError } from "./alert-error.js"
 
 //Varáveis - variables
 const form = document.querySelector("form");
@@ -15,9 +16,11 @@ form.onsubmit = (event) => {
   const showAlertError = notANumber(weight) || notANumber(height);
 
   if (showAlertError) {
-    console.log("mostrar erro");
+    AlertError.open()
     return;
   }
+
+  AlertError.close()
 
   const result = IMC(weight, height);
   const message = `Seu IMC é de ${result}`;
