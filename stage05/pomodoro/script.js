c82e3f3622c010f8e2dc04153217b816ea37a47f
João <joaovictor.ramos34@gmail.com>
2023-04-23 10:28:09 -0300

    Iniciando  a lógica do contador

--- stage05/pomodoro/script.js
+++ stage05/pomodoro/script.js
@@ -4,12 +4,16 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+let minutes;
 
 buttonPlay.addEventListener('click', start)
 buttonPause.addEventListener('click', pause)
 buttonStop.addEventListener('click', resetControls)
 buttonSoundOn.addEventListener('click', soundCheck)
 buttonSoundOff.addEventListener('click', soundCheck)
+buttonSet.addEventListener('click', setTime)
 
 
 function start() {
@@ -17,6 +21,8 @@ function start() {
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
+
+  countdown()
 }
 
 function pause() {
@@ -34,4 +40,22 @@ function resetControls(){
 function soundCheck() {
   buttonSoundOff.classList.toggle('hide')
   buttonSoundOn.classList.toggle('hide')
+}
+
+function setTime() {
+  minutes = Number(prompt('Quantos minutos? '))
+  minutesDisplay.textContent = minutes
+}
+
+function countdown() {
+  setTimeout(function() {
+    let seconds = Number(secondsDisplay.textContent)
+
+    if(seconds <= 0 ){
+      seconds = 60
+    }
+
+    secondsDisplay.textContent = seconds - 1
+    countdown()
+  }, 1000)
 }
