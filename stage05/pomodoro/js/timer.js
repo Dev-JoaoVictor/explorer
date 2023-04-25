f5277df6eddb40dc578630f4f0cd7e91ed40efb7
João <joaovictor.ramos34@gmail.com>
2023-04-25 09:43:21 -0300

    Ajustes no contador

--- stage05/pomodoro/js/timer.js
+++ stage05/pomodoro/js/timer.js
@@ -1,9 +1,4 @@
-export function Timer({
-  minutesDisplay,
-  secondsDisplay,
-  resetControls,
-}) {
-
+export function Timer({ minutesDisplay, secondsDisplay, resetControls }) {
   let timerTimeOut;
   let minutes = Number(minutesDisplay.textContent);
 
@@ -16,11 +11,13 @@ export function Timer({
     timerTimeOut = setTimeout(function () {
       let seconds = Number(secondsDisplay.textContent);
       let minutes = Number(minutesDisplay.textContent);
+      let isFinished = minutes <= 0 && seconds <= 0
 
       updateDisplay(minutes, 0);
 
-      if (minutes <= 0) {
+      if (isFinished) {
         resetControls();
+        updateDisplay();
         return;
       }
 
@@ -35,8 +32,10 @@ export function Timer({
     }, 1000);
   }
 
-  function updateDisplay(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, "0");
+  function updateDisplay(newMinutes, seconds) {
+    newMinutes = newMinutes === undefined ? minutes : newMinutes;
+    seconds = seconds === undefined ? 0 : seconds;
+    minutesDisplay.textContent = String(newMinutes).padStart(2, "0");
     secondsDisplay.textContent = String(seconds).padStart(2, "0");
   }
 
