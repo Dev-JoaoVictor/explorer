db6505a04f6c1ffb2fb59b95e0f560c4361d7760
João <joaovictor.ramos34@gmail.com>
2023-04-26 13:03:31 -0300

    ajustes

--- stage05/pomodoro2.0/js/index.js
+++ stage05/pomodoro2.0/js/index.js
@@ -2,10 +2,11 @@ const sun = document.querySelector(".sun");
 const moon = document.querySelector(".moon");
 const theme = document.documentElement;
 
-sun.addEventListener('click', Toggle)
-moon.addEventListener('click', Toggle)
 
-function Toggle() {
+sun.addEventListener('click', ToggleTheme)
+moon.addEventListener('click', ToggleTheme)
+
+function ToggleTheme() {
   sun.classList.toggle('hide')
   moon.classList.toggle('hide')
   theme.classList.toggle('dark')
