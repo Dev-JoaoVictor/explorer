464f1fdaec52053240c2eb90c5a836c0a4dd8335
João <joaovictor.ramos34@gmail.com>
2023-04-27 15:06:17 -0300

    Criando as primeiras funções

--- stage05/pomodoro2.0/js/index.js
+++ stage05/pomodoro2.0/js/index.js
@@ -2,12 +2,16 @@ const sun = document.querySelector(".sun");
 const moon = document.querySelector(".moon");
 const theme = document.documentElement;
 
+sun.addEventListener("click", ToggleTheme);
+moon.addEventListener("click", ToggleTheme);
 
-sun.addEventListener('click', ToggleTheme)
-moon.addEventListener('click', ToggleTheme)
-
-function ToggleTheme() {
-  sun.classList.toggle('hide')
-  moon.classList.toggle('hide')
-  theme.classList.toggle('dark')
+export function ToggleTheme() {
+  sun.classList.toggle("hide");
+  moon.classList.toggle("hide");
+  theme.classList.toggle("dark");
 }
+
+const cardList = document.querySelector(".container-cards");
+const card = cardList.querySelectorAll('.card')
+
+
