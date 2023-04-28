a430878578dbaad384f7379d791ae066658036c7
João <joaovictor.ramos34@gmail.com>
2023-04-28 12:05:08 -0300

    adicionando o botão de pause

--- stage05/pomodoro2.0/js/index.js
+++ stage05/pomodoro2.0/js/index.js
@@ -1,1 +1,13 @@
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
 
+buttonPlay.addEventListener("click", () => {
+  buttonPlay.classList.toggle('hide')
+  buttonPause.classList.toggle('hide')
+})
+
+buttonPause.addEventListener("click", () => {
+  buttonPlay.classList.toggle('hide')
+  buttonPause.classList.toggle('hide')
+})
