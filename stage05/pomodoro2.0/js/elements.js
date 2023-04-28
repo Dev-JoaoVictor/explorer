69988b469078060f615fb2c73d7b5f48b932bd4a
João <joaovictor.ramos34@gmail.com>
2023-04-28 11:20:37 -0300

    Resetando o projeto

--- stage05/pomodoro2.0/js/elements.js
+++
@@ -1,8 +0,0 @@
-const sun = document.querySelector(".sun");
-const moon = document.querySelector(".moon");
-
-export {
-  sun,
-  moon,
-}
-
--- stage05/pomodoro2.0/js/index.js
+++ stage05/pomodoro2.0/js/index.js
@@ -1,10 +1,1 @@
-import { Theme } from "./theme.toggle.js";
-import { sun, moon } from "./elements.js";
 
-const configTheme = Theme({
-  sun,
-  moon,
-});
-
-sun.addEventListener("click", configTheme.ToggleTheme);
-moon.addEventListener("click", configTheme.ToggleTheme);
