b95f98c58abded184b729fadbbd40ec2f62e2baf
João <joaovictor.ramos34@gmail.com>
2023-05-03 16:33:32 -0300

    Orientação e objeto

--- stage06/js/index.js
+++ stage06/js/index.js
@@ -1,3 +1,5 @@
+import './router.js'
+
 const routes = {
   "/": "/pages/home.html",
   "/about": "/pages/about.html",
@@ -5,28 +7,7 @@ const routes = {
   404: "/pages/404.html",
 };
 
-function route(event) {
-  event = event || window.event;
-  event.preventDefault();
+handle();
 
-  window.history.pushState({}, "", event.target.href);
-
-  handle();
-}
-
-function handle() {
-  const { pathname } = window.location;
-  const route = routes[pathname] || routes[404];
-
-  fetch(route)
-    .then((data) => data.text())
-    .then((html) => {
-      document.querySelector('.app').innerHTML = html
-    });
-  
-}
-
-handle()
-
-window.onpopstate = () => handle()
-window.route = () => route()
+window.onpopstate = () => handle();
+window.route = () => route();
