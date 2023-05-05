4433f223ceeafb14d9d7cb96cf97e3d6705661e1
João <joaovictor.ramos34@gmail.com>
2023-05-05 11:55:48 -0300

    terminando o projeto

--- stage06/desafio/js/index.js
+++ stage06/desafio/js/index.js
@@ -1,17 +1,33 @@
 import { Router } from "./router.js";
 
-const linkHome = document.querySelector('.link-home')
-const linkUniver = document.querySelector('.link-universe')
-const linkExplorer = document.querySelector('.link-explorer')
-const body = document.querySelector('body')
+const linkHome = document.querySelector(".link-home");
+const linkUniver = document.querySelector(".link-universe");
+const linkExplorer = document.querySelector(".link-explorer");
+const body = document.querySelector("body");
 
+function removeBackground() {
+  body.classList.remove("body-universe");
+  body.classList.remove("body-explorer");
+}
 
+linkHome.addEventListener("click", () => {
+  removeBackground();
+});
+
+linkUniver.addEventListener("click", () => {
+  body.classList.add("body-universe");
+  body.classList.remove("body-explorer");
+});
+
+linkExplorer.addEventListener("click", () => {
+  body.classList.remove("body-universe");
+  body.classList.add("body-explorer");
+});
 
 const router = new Router();
 router.add("/", "/pages/home.html");
 router.add("/universe", "/pages/universe.html");
 router.add("/explorer", "/pages/explorer.html");
-
 
 router.handle();
 
