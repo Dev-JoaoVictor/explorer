d6c0fa7b7e262445f9123aa87e860bb7f8db0a3a
João <joaovictor.ramos34@gmail.com>
2023-05-06 14:18:29 -0300

    Criando a função de Criar linhas

--- stage06/Promisses/js/favortites.js
+++ stage06/Promisses/js/favortites.js
@@ -14,6 +14,30 @@ export class FavoritesView extends Favorites {
     this.removeAllTr();
   }
 
+  createRow() {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+    <td class="user">
+      <img
+        src="https://github.com/Dev-JoaoVictor.png"
+        alt="Imagem de João victor"
+      />
+      <a href="https://github.com/Dev-JoaoVictor" target="_blank">
+        <p>João Victor</p>
+        <span>Dev-JoaoVictor</span>
+      </a>
+    </td>
+    <td class="repositories">53</td>
+    <td class="followers">53</td>
+    <td>
+      <button class="remove">&times;</button>
+    </td>
+  `;
+
+    return tr;
+  }
+
   removeAllTr() {
     const tbody = this.root.querySelector("table tbody");
 
