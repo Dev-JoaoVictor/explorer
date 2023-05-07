0e5eca4f4e70676b938664a8eba44c2aa72a5b42
João <joaovictor.ramos34@gmail.com>
2023-05-07 12:33:46 -0300

    Consumindo a api do github

--- stage06/Promisses/js/favortites.js
+++ stage06/Promisses/js/favortites.js
@@ -1,11 +1,28 @@
+export class GithubUser {
+  static search(username) {
+    const endpoint = `https://api.github.com/users/${username}`;
+
+    return fetch(endpoint)
+      .then((data) => data.json())
+      .then(({ login, name, public_repos, followers }) => ({
+        login,
+        name,
+        public_repos,
+        followers,
+      }));
+  }
+}
+
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
     this.load();
+
+    GithubUser.search('Dev-JoaoVictor').then(user => console.log(user))
   }
 
   load() {
-    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+    this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || [];
   }
 
   delete(user) {
@@ -13,8 +30,8 @@ export class Favorites {
       (entry) => entry.login !== user.login
     );
 
-    this.entries = filteredEntries
-    this.update()
+    this.entries = filteredEntries;
+    this.update();
   }
 }
 
