741fe3be4be913c0224243d0987ea49cb6c3cd43
João <joaovictor.ramos34@gmail.com>
2023-05-08 10:22:14 -0300

    Separando a função GithubUser

--- stage06/Promisses/js/favortites.js
+++ stage06/Promisses/js/favortites.js
@@ -1,17 +1,4 @@
-export class GithubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`;
-
-    return fetch(endpoint)
-      .then((data) => data.json())
-      .then(({ login, name, public_repos, followers }) => ({
-        login,
-        name,
-        public_repos,
-        followers,
-      }));
-  }
-}
+import { GithubUser } from './GithubUser.js'
 
 export class Favorites {
   constructor(root) {
