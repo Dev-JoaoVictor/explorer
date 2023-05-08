6669474bb6ec6ad38ea91d2ec6b2febe6e457694
João <joaovictor.ramos34@gmail.com>
2023-05-08 10:27:59 -0300

    Modificando o link do perfil adicionado

--- stage06/Promisses/js/favortites.js
+++ stage06/Promisses/js/favortites.js
@@ -79,6 +79,7 @@ export class FavoritesView extends Favorites {
         ".user img"
       ).src = `https://github.com/${user.login}.png`;
       row.querySelector(".user img").alt = `imagem de ${user.name}`;
+      row.querySelector(".user a").href = `https://github.com/${user.login}`;
       row.querySelector(".user p").textContent = user.name;
       row.querySelector(".user span").textContent = user.login;
       row.querySelector(".repositories").textContent = user.public_repos;
