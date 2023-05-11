b9c1130c19d24e11de03415b913906e5e2f1d151
João <joaovictor.ramos34@gmail.com>
2023-05-11 15:28:51 -0300

    Utilizando o Params

--- stage08/src/server.js
+++ stage08/src/server.js
@@ -2,8 +2,13 @@ const express = require("express"); // importando express
 
 const app = express(); //iniciando express
 
-app.get("/", (request, response) => {
-  response.send("Hello, world") // envia uma resposta pra quem solicitou a rota
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params;
+
+  response.send(`
+    Id da mensagem ${id}.
+    Para o usuário: ${user}.
+  `) // envia uma resposta pra quem solicitou a rota
 })
 
 const PORT = 3333; //número da porta que será observada
