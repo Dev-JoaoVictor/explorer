a0c400c7a4de944175b00a5f05cb1445b8265e66
João <joaovictor.ramos34@gmail.com>
2023-05-11 15:11:59 -0300

    utilizando o método GET

--- stage08/src/server.js
+++ stage08/src/server.js
@@ -2,6 +2,10 @@ const express = require("express"); // importando express
 
 const app = express(); //iniciando express
 
+app.get("/", (request, response) => {
+  response.send("Hello, world") // envia uma resposta pra quem solicitou a rota
+})
+
 const PORT = 3333; //número da porta que será observada
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
