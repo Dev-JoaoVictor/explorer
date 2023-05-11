f66ee0e861cbc86bbf5d70377796409d71997280
João <joaovictor.ramos34@gmail.com>
2023-05-11 11:10:40 -0300

    ajustes

--- stage06/Promisses/js/favortites.js
+++ stage06/Promisses/js/favortites.js
@@ -81,7 +81,7 @@ export class FavoritesView extends Favorites {
       row.querySelector(".user img").alt = `imagem de ${user.name}`;
       row.querySelector(".user a").href = `https://github.com/${user.login}`;
       row.querySelector(".user p").textContent = user.name;
-      row.querySelector(".user span").textContent = user.login;
+      row.querySelector(".user span ").textContent = user.login;
       row.querySelector(".repositories").textContent = user.public_repos;
       row.querySelector(".followers").textContent = user.followers;
       row.querySelector(".remove").onclick = () => {
