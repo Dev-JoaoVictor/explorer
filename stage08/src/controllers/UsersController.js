6d1b2a3f493d54f928aeee872aa6718fd6dc323f
João <joaovictor.ramos34@gmail.com>
2023-05-12 11:24:11 -0300

    aprendendo o padrão do controller

---
+++ stage08/src/controllers/UsersController.js
@@ -0,0 +1,11 @@
+class UsersController {
+  /** O controller poder ter no máximo 5 métodos
+   * index - GET para listar vários registros.
+   * show - GET para exibir um registro específico.
+   * create - POST para criar um registro.
+   * update - PUT para atualizar um registro.
+   * delete - DELETE para remover um registro.
+   */
+}
+
+module.exports = UsersController;
