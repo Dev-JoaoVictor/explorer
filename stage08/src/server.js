5ded044905833ea81264e366cda67946a88777bd
João <joaovictor.ramos34@gmail.com>
2023-05-12 12:53:21 -0300

    tratamento de execessões

--- stage08/src/server.js
+++ stage08/src/server.js
@@ -1,9 +1,28 @@
-const express = require("express"); // importando express
-const routes = require("./routes")
-const app = express(); //iniciando express
+require("express-async-errors");
 
+const AppError = require("./utils/AppError");
+const express = require("express"); // importando express
+const routes = require("./routes");
+
+const app = express(); //iniciando express
 app.use(express.json());
-app.use(routes)
+app.use(routes);
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.error(error)
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 const PORT = 3333; //número da porta que será observada
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
