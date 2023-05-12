e373370f65b3978c4c370d8fafadb2b79612c6a4
João <joaovictor.ramos34@gmail.com>
2023-05-12 12:17:50 -0300

    Utilizando middleware

--- stage08/src/routes/users.routes.js
+++ stage08/src/routes/users.routes.js
@@ -4,8 +4,17 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-const  usersController = new UsersController()
+function myMiddleware(request, response, next) {
+  console.log("Você passou pelo Middleware");
 
-usersRoutes.post("/", usersController.create);
+  if (!request.body.isAdmin) {
+    return response.json({ message: "User unauthorized" });
+  }
+  next();
+}
 
+const usersController = new UsersController();
+
+// usersRoutes.use(myMiddleware) - caso queira utilizar em todas as rotas
+usersRoutes.post("/", usersController.create); 
 module.exports = usersRoutes;
