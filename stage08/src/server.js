779c42a6b65be2fb0f0787e6c88a99803133df27
João <joaovictor.ramos34@gmail.com>
2023-05-12 10:34:54 -0300

    Utilizando o metodo POST

--- stage08/src/server.js
+++ stage08/src/server.js
@@ -2,19 +2,8 @@ const express = require("express"); // importando express
 
 const app = express(); //iniciando express
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
-
-  response.send(`
-    Id da mensagem ${id}.
-    Para o usuário: ${user}.
-  `); // envia uma resposta pra quem solicitou a rota
-});
-
-app.get("/users", (resquest, response) => {
-  const { page, limit } = resquest.query;
-
-  response.send(`Página: ${page}. Mostrar: ${limit}`);
+app.post("/users", (resquest, response) => {
+  response.send(`Você chamou o POST`);
 });
 
 const PORT = 3333; //número da porta que será observada
