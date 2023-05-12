e50fcfa1163e229ef4619ff9d97239a097f3880b
João <joaovictor.ramos34@gmail.com>
2023-05-12 12:30:27 -0300

    Construindo a classe de Erro

--- stage08/src/routes/users.routes.js
+++ stage08/src/routes/users.routes.js
@@ -4,15 +4,6 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-function myMiddleware(request, response, next) {
-  console.log("Você passou pelo Middleware");
-
-  if (!request.body.isAdmin) {
-    return response.json({ message: "User unauthorized" });
-  }
-  next();
-}
-
 const usersController = new UsersController();
 
 // usersRoutes.use(myMiddleware) - caso queira utilizar em todas as rotas
