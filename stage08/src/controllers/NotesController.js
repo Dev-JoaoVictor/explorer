66a5cdb3984f974ab5502a855e971f32a3384dd3
João <joaovictor.ramos34@gmail.com>
2023-05-17 15:45:28 -0300

    Listando tags

--- stage08/src/controllers/NotesController.js
+++ stage08/src/controllers/NotesController.js
@@ -66,7 +66,13 @@ class NotesController {
       const filterTags = tags.split(",").map((tag) => tag.trim());
       console.log(filterTags);
 
-      notes = await knex("tags").whereIn("name", filterTags);
+      notes = await knex("tags")
+        .select(["notes.id", "notes.title", "notes.user_id"])
+        .where("notes.user_id", user_id)
+        .whereLike("notes.title", `%${title}%`)
+        .whereIn("name", filterTags)
+        .innerJoin("notes", "notes.id", "tags.note_id")
+        .orderBy("notes.title")
     } else {
       notes = await knex("notes")
         .where({ user_id })
@@ -74,7 +80,17 @@ class NotesController {
         .orderBy("title");
     }
 
-    return response.json(notes);
+    const userTags = await knex("tags").where({ user_id });
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
+
+    return response.json(notesWithTags);
   }
 }
 
