cc422258ebbe74b7057cc62345e5c338dc5ab9c5
João <joaovictor.ramos34@gmail.com>
2023-05-17 14:22:33 -0300

    função de listar notas cadastradas

--- stage08/src/routes/notes.routes.js
+++ stage08/src/routes/notes.routes.js
@@ -7,6 +7,7 @@ const notesRoutes = Router();
 const notesController = new NotesController();
 
 // usersRoutes.use(myMiddleware) - caso queira utilizar em todas as rotas
+notesRoutes.get("/", notesController.index);
 notesRoutes.post("/:user_id", notesController.create);
 notesRoutes.get("/:id", notesController.show);
 notesRoutes.delete("/:id", notesController.delete);
