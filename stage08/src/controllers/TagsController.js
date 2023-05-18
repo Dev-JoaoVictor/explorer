44814d922260e2cafcb6257655a76dcdf28943ea
João <joaovictor.ramos34@gmail.com>
2023-05-18 08:55:37 -0300

    Listando as tags

--- stage08/src/controllers/TagsController.js
+++ stage08/src/controllers/TagsController.js
@@ -1,9 +1,8 @@
-const { response } = require("express");
 const knex = require("../database/knex")
 
 class TagsController {
-  async index(request,reponse) {
-    const user_id = request.params;
+  async index(request,response) {
+    const { user_id } = request.params;
 
     const tags = await knex("tags").where({ user_id })
     return response.json(tags)
