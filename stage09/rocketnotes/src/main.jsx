7983d0565d1c5f989c9e55396ab769ebf77d10ba
João <joaovictor.ramos34@gmail.com>
2023-05-23 14:56:19 -0300

    arquivo de estilização global

--- stage09/rocketnotes/src/main.jsx
+++ stage09/rocketnotes/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Details } from "./pages/Details";
+import GlobalStyles from './styles/global'
 import { ThemeProvider } from "styled-components";
 
 import theme from "./styles/theme";
@@ -8,6 +9,7 @@ import theme from "./styles/theme";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles/>
       <Details />
     </ThemeProvider>
   </React.StrictMode>
