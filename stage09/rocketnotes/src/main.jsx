1a4713c8da7ceb96c450c3ed6e4f0aabccddf698
João <joaovictor.ramos34@gmail.com>
2023-05-23 11:01:37 -0300

    Adicionando o css

--- stage09/rocketnotes/src/main.jsx
+++ stage09/rocketnotes/src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Details } from "./pages/Details";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Details />
+  </React.StrictMode>
+);
---
+++ stage09/rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css"
+
+export function Details() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <span>João Victor</span>
+    </div>
+  )
+}
