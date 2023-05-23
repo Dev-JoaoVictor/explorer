e794ceea23331e0bed5e4e5f25e3cc5b925752bc
João <joaovictor.ramos34@gmail.com>
2023-05-23 15:23:46 -0300

    Criando o botão

---
+++ stage09/rocketnotes/src/components/Button/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+
+export function Button() {
+  return <Container type="button">Meu Botão</Container>;
+}
--- stage09/rocketnotes/src/pages/Details/index.jsx
+++ stage09/rocketnotes/src/pages/Details/index.jsx
@@ -1,10 +1,13 @@
-import { Container } from './styles'
+import { Container } from "./styles";
+
+import { Button } from "../../components/Button";
 
 export function Details() {
   return (
     <Container>
       <h1>Hello world!</h1>
       <span>João Victor</span>
+      <Button />
     </Container>
-  )
+  );
 }
