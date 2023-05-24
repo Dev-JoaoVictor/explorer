f32feca10ee9f58469d2e280d378b82ad10ac59c
João <joaovictor.ramos34@gmail.com>
2023-05-24 10:37:40 -0300

    Criando a seção de links

--- stage09/rocketnotes/src/pages/Details/index.jsx
+++ stage09/rocketnotes/src/pages/Details/index.jsx
@@ -1,12 +1,19 @@
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
 
 export function Details() {
   return (
     <Container>
       <Header/>
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">Linkteste@gmail.com</a></li>
+          <li><a href="#">Linkteste2@gmail.com</a></li>
+        </Links>
+      </Section>
       <Button title="Voltar"/>
     </Container>
   );
--- stage09/rocketnotes/src/pages/Details/styles.js
+++ stage09/rocketnotes/src/pages/Details/styles.js
@@ -6,8 +6,19 @@ export const Container = styled.div`
 
   display: grid;
   grid-template-rows: 105px auto;
-  grid-template-areas: 
-  "header"
-  "content" 
-  ;
+  grid-template-areas:
+    "header"
+    "content";
+`;
+
+export const Links = styled.ul`
+  list-style: none;
+
+  > li {
+    margin-top: 12px;
+  }
+
+  a {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  }
 `;
