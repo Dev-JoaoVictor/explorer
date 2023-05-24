84eae6d0876e72f03bd01379ba77665cf4258e6b
João <joaovictor.ramos34@gmail.com>
2023-05-24 11:07:59 -0300

    Interface de detalhes

--- stage09/rocketnotes/src/pages/Details/index.jsx
+++ stage09/rocketnotes/src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./styles";
+import { Container, Links, Content } from "./styles";
 
 import { Tag } from "../../components/Tag";
 import { Header } from "../../components/Header";
@@ -9,21 +9,38 @@ import { ButtonText } from "../../components/ButtonText";
 export function Details() {
   return (
     <Container>
-      <Header/>
-      <ButtonText title="Excluir nota"/>
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">Linkteste@gmail.com</a></li>
-          <li><a href="#">Linkteste2@gmail.com</a></li>
-        </Links>
-      </Section>
+      <Header />
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure
+            inventore, quisquam cum eligendi explicabo nobis culpa porro debitis
+            aut eos molestias cupiditate quae dignissimos reiciendis. Sint,
+            molestiae aliquid quia odio veniam ab delectus eos facilis
+            cupiditate, culpa ipsam. Vero sapiente laborum omnis officiis
+            temporibus eos hic aliquid. Rem, natus. Magni.
+          </p>
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">Linkteste@gmail.com</a>
+              </li>
+              <li>
+                <a href="#">Linkteste2@gmail.com</a>
+              </li>
+            </Links>
+          </Section>
 
-      <Section title="Marcadores">
-        <Tag title="node"/>
-        <Tag title="express"/>
-      </Section>
+          <Section title="Marcadores">
+            <Tag title="node" />
+            <Tag title="express" />
+          </Section>
 
-      <Button title="Voltar"/>
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
