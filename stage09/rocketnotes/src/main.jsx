8f6fa1359b2a327e97194102b61253554c9bbbce
João <joaovictor.ramos34@gmail.com>
2023-05-25 12:06:46 -0300

    construindo a home

--- stage09/rocketnotes/src/main.jsx
+++ stage09/rocketnotes/src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Details } from "./pages/Details";
 import GlobalStyles from './styles/global'
 import { ThemeProvider } from "styled-components";
 
 import theme from "./styles/theme";
 
+import { Home } from "./pages/Home";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 );
