0e0100dd7f6d80f84e34f182493c9959b353ae19
João <joaovictor.ramos34@gmail.com>
2023-05-26 10:32:13 -0300

    Criando a tela de SignIn

--- stage09/rocketnotes/src/main.jsx
+++ stage09/rocketnotes/src/main.jsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from "styled-components";
 
 import theme from "./styles/theme";
 
-import { Home } from "./pages/Home";
+import { SignIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 );
