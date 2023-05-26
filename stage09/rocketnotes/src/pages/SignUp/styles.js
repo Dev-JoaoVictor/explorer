b7a69c9996d13558c26453354586941f4540dc33
João <joaovictor.ramos34@gmail.com>
2023-05-26 10:44:09 -0300

    Criando a tela de SignUp

--- stage09/rocketnotes/src/pages/SignUp/styles.js
+++ stage09/rocketnotes/src/pages/SignUp/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import backgroundImg from "../../assets/background.png"
+import backgroundImg from "../../assets/background.png";
 
 export const Container = styled.div`
   height: 100vh;
@@ -42,5 +42,5 @@ export const Form = styled.form`
 export const Background = styled.div`
   flex: 1;
   background: url(${backgroundImg}) no-repeat center center;
-  background-size: cov;
+  background-size: cover;
 `;
