a2da1ab591cc4b26c5aa95c0e18ce0aac1183d57
João <joaovictor.ramos34@gmail.com>
2023-05-26 14:08:42 -0300

    Finalizando a page New

--- stage09/rocketnotes/src/pages/New/index.jsx
+++ stage09/rocketnotes/src/pages/New/index.jsx
@@ -1,5 +1,6 @@
-import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
 import { Textarea } from "../../components/Textarea";
 import { NoteItem } from "../../components/NoteItem";
 import { Section } from "../../components/Section";
@@ -16,11 +17,22 @@ export function New() {
             <h1>Criar nota</h1>
             <a href="/">voltar</a>
           </header>
+
           <Input placeholder="Titulo" />
-          <Textarea placeholder="Observações"/>
-          <Section title="Links úteis"/>
-          <NoteItem value="https://rockeseat.com.br"/>
-          <NoteItem isNew placeholder="Novo link"/>
+          <Textarea placeholder="Observações" />
+
+          <Section title="Links úteis">
+            <NoteItem value="https://rockeseat.com.br" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="React" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
