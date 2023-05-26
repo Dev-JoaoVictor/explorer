d11d01742b4342e36ab98d01893eac59294a2b29
João <joaovictor.ramos34@gmail.com>
2023-05-26 13:37:04 -0300

    Criando Componente textarea

--- stage09/rocketnotes/src/main.jsx
+++ stage09/rocketnotes/src/main.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GlobalStyles from './styles/global'
+import GlobalStyles from "./styles/global";
 import { ThemeProvider } from "styled-components";
 
 import theme from "./styles/theme";
 
-import { Profile } from './pages/Profile'
+import { New } from "./pages/New";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles/>
-      <Profile />
+      <GlobalStyles />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ stage09/rocketnotes/src/pages/New/index.jsx
@@ -0,0 +1,23 @@
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Textarea } from "../../components/Textarea";
+
+import { Container, Form } from "./styles";
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+          <Input placeholder="Titulo" />
+          <Textarea placeholder="Observações"/>
+        </Form>
+      </main>
+    </Container>
+  );
+}
