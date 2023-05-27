216833e0e73b9182150e37f9a1d391f212d39ec3
João <joaovictor.ramos34@gmail.com>
2023-05-27 10:57:15 -0300

    Gerando token do usuário

--- stage08/src/controllers/SessionsController.js
+++ stage08/src/controllers/SessionsController.js
@@ -1,5 +1,8 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
+
 const { compare } = require("bcryptjs");
 
 class SessionsController {
@@ -18,7 +21,13 @@ class SessionsController {
       throw new AppError("E-mail e/ou senha incorreta", 401);
     }
 
-    return response.json({ user });
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn,
+    });
+
+    return response.json({ user, token });
   }
 }
 
