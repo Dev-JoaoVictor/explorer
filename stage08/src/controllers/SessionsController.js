ddd790c9509d17fff536cd5e3d1710306d425fef
João <joaovictor.ramos34@gmail.com>
2023-05-27 10:33:51 -0300

    validação de senha

--- stage08/src/controllers/SessionsController.js
+++ stage08/src/controllers/SessionsController.js
@@ -1,14 +1,21 @@
-const knex = require("../database/knex")
-const AppError = require("../utils/AppError")
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
 
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body;
 
-    const user = await knex("users").where({email}).first();
+    const user = await knex("users").where({ email }).first();
 
-    if(!user) {
-      throw new AppError("E-mail e/ou senha incorreta", 401)
+    if (!user) {
+      throw new AppError("E-mail e/ou senha incorreta", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("E-mail e/ou senha incorreta", 401);
     }
 
     return response.json({ user });
