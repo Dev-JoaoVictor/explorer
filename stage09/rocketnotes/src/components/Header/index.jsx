5e4cbd94c136bbd6bc0d91744e2b348dc053a67b
João <joaovictor.ramos34@gmail.com>
2023-06-04 13:10:03 -0300

    Criando a função logout

--- stage09/rocketnotes/src/components/Header/index.jsx
+++ stage09/rocketnotes/src/components/Header/index.jsx
@@ -1,12 +1,14 @@
-import { RiShutDownLine } from 'react-icons/ri'
-import { Container, Profile, Logout } from './styles'
+import { RiShutDownLine } from "react-icons/ri";
+import { Container, Profile, Logout } from "./styles";
+import { useAuth } from "../../hooks/auth";
 
 export function Header() {
+  const { signOut } = useAuth();
   return (
     <Container>
       <Profile to="/profile">
-        <img 
-          src="https://github.com/Dev-JoaoVictor.png" 
+        <img
+          src="https://github.com/Dev-JoaoVictor.png"
           alt="Foto do usuário"
         />
         <div>
@@ -15,9 +17,9 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout>
-        <RiShutDownLine/>
+      <Logout onClick={signOut}>
+        <RiShutDownLine />
       </Logout>
     </Container>
-  )
+  );
 }
