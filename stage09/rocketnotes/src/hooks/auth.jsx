900cb80c68533a6ded74373049badee7b19397cf
João <joaovictor.ramos34@gmail.com>
2023-06-05 10:56:23 -0300

    Atualizando o perfil do usuário

--- stage09/rocketnotes/src/hooks/auth.jsx
+++ stage09/rocketnotes/src/hooks/auth.jsx
@@ -34,6 +34,22 @@ function AuthProvider({ children }) {
     setData({});
   }
 
+  async function updateProfile({ user }) {
+    try {
+      await api.put("users", user);
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+
+      setData({ user, token: data.token });
+      alert("Perfil atualizado");
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível atualizar o perfil.");
+      }
+    }
+  }
+
   useEffect(() => {
     const token = localStorage.getItem("@rocketnotes:token");
     const user = localStorage.getItem("@rocketnotes:user");
@@ -53,6 +69,7 @@ function AuthProvider({ children }) {
       value={{
         signIn,
         signOut,
+        updateProfile,
         user: data.user,
       }}
     >
