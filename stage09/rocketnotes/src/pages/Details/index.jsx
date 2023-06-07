7b7ae64eb2e3bd470dfa2cfc792b0b3405b74dd5
João <joaovictor.ramos34@gmail.com>
2023-06-07 11:09:13 -0300

    Carregando detalhes da nota

--- stage09/rocketnotes/src/pages/Details/index.jsx
+++ stage09/rocketnotes/src/pages/Details/index.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react";
 import { Container, Links, Content } from "./styles";
+
+import { api } from "../../service/api";
+import { useParams, useNavigate } from "react-router-dom";
 
 import { Tag } from "../../components/Tag";
 import { Header } from "../../components/Header";
@@ -7,40 +11,59 @@ import { Section } from "../../components/Section";
 import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate("/");
+  }
+
+  useEffect(() => {
+    async function fetchNotes() {
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNotes();
+  }, []);
+
   return (
     <Container>
       <Header />
-      <main>
-        <Content>
-          <ButtonText title="Excluir nota" />
-          <h1>Introdução ao React</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure
-            inventore, quisquam cum eligendi explicabo nobis culpa porro debitis
-            aut eos molestias cupiditate quae dignissimos reiciendis. Sint,
-            molestiae aliquid quia odio veniam ab delectus eos facilis
-            cupiditate, culpa ipsam. Vero sapiente laborum omnis officiis
-            temporibus eos hic aliquid. Rem, natus. Magni.
-          </p>
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href="#">Linkteste@gmail.com</a>
-              </li>
-              <li>
-                <a href="#">Linkteste2@gmail.com</a>
-              </li>
-            </Links>
-          </Section>
+      {data && (
+        <main>
+          <Content>
+            <ButtonText title="Excluir nota" />
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+            {data.links && (
+              <Section title="Links úteis">
+                <Links>
+                  {data.links.map((link) => (
+                    <li key={String(link.id)}>
+                      <a href={link.url} target="_blank">
+                        {link.url}
+                      </a>
+                    </li>
+                  ))}
+                </Links>
+              </Section>
+            )}
 
-          <Section title="Marcadores">
-            <Tag title="node" />
-            <Tag title="express" />
-          </Section>
+            {data.tags && (
+              <Section title="Marcadores">
+                {data.tags.map((tag) => (
+                  <Tag key={String(tag.id)} title={tag.name} />
+                ))}
+              </Section>
+            )}
 
-          <Button title="Voltar" />
-        </Content>
-      </main>
+            <Button title="Voltar" onClick={handleBack} />
+          </Content>
+        </main>
+      )}
     </Container>
   );
 }
