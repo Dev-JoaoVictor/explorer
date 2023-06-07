093ea877fac4df961a3b6846cf92d26f50a045ad
João <joaovictor.ramos34@gmail.com>
2023-06-07 11:13:02 -0300

    Removendo notas

--- stage09/rocketnotes/src/pages/Details/index.jsx
+++ stage09/rocketnotes/src/pages/Details/index.jsx
@@ -20,6 +20,15 @@ export function Details() {
     navigate("/");
   }
 
+  async function handleRemove() {
+    const confirm = window.confirm("Deseja realmente remover a nota?");
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`);
+      navigate("/");
+    }
+  }
+
   useEffect(() => {
     async function fetchNotes() {
       const response = await api.get(`/notes/${params.id}`);
@@ -35,7 +44,7 @@ export function Details() {
       {data && (
         <main>
           <Content>
-            <ButtonText title="Excluir nota" />
+            <ButtonText title="Excluir nota" onClick={handleRemove} />
             <h1>{data.title}</h1>
             <p>{data.description}</p>
             {data.links && (
