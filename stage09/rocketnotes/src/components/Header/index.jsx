886199a9e71982fc2d26dc7f4d57246bbfecb955
João <joaovictor.ramos34@gmail.com>
2023-06-07 14:38:22 -0300

    correção no logout

--- stage09/rocketnotes/src/components/Header/index.jsx
+++ stage09/rocketnotes/src/components/Header/index.jsx
@@ -2,28 +2,34 @@ import { RiShutDownLine } from "react-icons/ri";
 import { Container, Profile, Logout } from "./styles";
 import { useAuth } from "../../hooks/auth";
 
-import  avatarPlaceHolder  from '../../assets/avatar_placeholder.svg'
 import { api } from "../../service/api";
+import { useNavigate } from "react-router-dom";
+import avatarPlaceHolder from "../../assets/avatar_placeholder.svg";
 
 export function Header() {
-  const { signOut, user} = useAuth();
+  const { signOut, user } = useAuth();
+  const navigation = useNavigate();
 
-  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceHolder;
+  function handleSignOut() {
+    navigation("/");
+    signOut();
+  }
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceHolder;
 
   return (
     <Container>
       <Profile to="/profile">
-        <img
-          src={avatarUrl}
-          alt={user.name}
-        />
+        <img src={avatarUrl} alt={user.name} />
         <div>
           <span>Bem vindo,</span>
           <strong>{user.name}</strong>
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
